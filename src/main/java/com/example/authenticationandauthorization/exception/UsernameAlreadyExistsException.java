becf9740409ba09f9cc4ee9da8720c209aa45d0e
shamin <shamin.asfaq@gmail.com>
2025-04-08 05:35:09 +0200

    first commit

--- src/main/java/com/example/authenticationandauthorization/exception/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.authenticationandauthorization.exception;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/authenticationandauthorization/model/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.authenticationandauthorization.model.enums;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MODERATOR
-}
